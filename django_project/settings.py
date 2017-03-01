848f1ad2c17a706d40067ac555c3486a73958e4c
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-01 14:42:12 +0530

    E

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 )
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 
 MIDDLEWARE_CLASSES = (
