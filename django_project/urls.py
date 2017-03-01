6ff98673d1849910a3e03c389a78080343978754
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-01 15:14:34 +0530

    E

--- django_project/urls.py
+++ django_project/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 from django_project.views import home
@@ -22,4 +20,4 @@ from django_project.views import home
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
