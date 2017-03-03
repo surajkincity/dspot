8d2a389130482a6acfc4bf2e437ad41403add3d5
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-03 15:47:48 +0530

    Zd

--- django_project/settings.py
+++ django_project/settings.py
@@ -104,7 +104,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 PROJECT_DIR  = os.path.dirname(__file__)
 
-
+MEDIA_ROOT = os.path.join(PROJECT_DIR,'media')
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
 STATIC_URL = '/static/'
 
