dbc6e94686de105eb73cf8bfce7ee8caaca8ac7e
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-03 15:29:40 +0530

    D

--- django_project/settings.py
+++ django_project/settings.py
@@ -102,8 +102,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_URL = 'http://static.139.59.42.123/'
-STATIC_ROOT = "/var/www/spotable.in/static"
+STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/spotable.in"
 STATICFILES_DIRS = (
     "/home/django/django_project/static",
 )
