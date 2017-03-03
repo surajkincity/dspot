f52aaa5e7162e767de99cd8cf8e166dc848b2072
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-03 14:53:54 +0530

    Ce

--- django_project/settings.py
+++ django_project/settings.py
@@ -102,7 +102,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = '/var/www/spotable.in/static/'
+STATIC_ROOT = "/var/www/spotable.in/static/"
+
 STATIC_URL = '/static/'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # Allow Django from all hosts. This snippet is installed from
