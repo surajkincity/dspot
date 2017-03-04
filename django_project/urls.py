62dfcda2064dc55f1e5470da3e52ae880edb37af
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-04 13:04:50 +0530

    Ce

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django_project.views import home,about,contact,privacy,sitemap,google
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 
 
