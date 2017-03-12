ea242f7435c06177d3be1aeb78854ac5f1f03e95
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-12 15:16:30 +0530

    D

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
-from blog.models import blog,comment
+
 from . import views
 
-
 urlpatterns = [
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
 ]
