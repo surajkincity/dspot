b3fa39c51ac0379d430fb2b26e64e2ff413a606e
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-03-12 15:10:02 +0530

    D

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,19 +3,19 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django_project.views import home,about,contact,privacy,sitemap,google
 from django.conf.urls import patterns, url
-from blog.views import
+from blog import views
 from blog.models import blog,comment
 
 
 urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
     url(r'^$', home , name='index'),
     url(r'^sitemap.xml/', sitemap , name='sitemap'),
     url(r'^googlefa975541a78f7cc0.html/', google , name='google'),
     url(r'^about-spotable/', about , name='about'),
     url(r'^contact-spotable/', contact , name='contact'),
     url(r'^pricavy-policy-of-spotable/', privacy , name='privacy'),
+    url(r'^blog/', include('blog.urls', namespace="blog")),
 
 
 )
